9aecd11b34c5ca34d5733b2042c6446e0a049629
Vitor Santos <vitorpedrosasantos@gmail.com>
2022-09-05 19:44:06 -0300

    Added CreateUser migrations (Error with MongoDB connection)

---
+++ src/database/migrations/1662401892227-CreateUsers.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+
+export class CreateUsers1662401892227 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await   queryRunner.query('CREATE EXNTENSION IF NOT EXISTS "uuid-ossp"')
+
+        // Creating user table
+        
+        await queryRunner.createTable(new Table({
+            name:   "users",
+            columns:    [
+                {
+                    name:   "id",
+                    type:   "uuid",
+                    isPrimary:  true,
+                    generationStrategy: "uuid",
+                    default:    "uuid_generate_v4()",
+                },
+                {
+                    name:   "email",
+                    "type": "varchar",
+                },
+             ],
+        }))
+    
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await   queryRunner.dropTable("users")
+        await   queryRunner.query("DROP EXTENSION 'uuid-ossp'")
+    }
+
+}

81cb4fb5682846eec6a01ed37e3a683188fda090
Vitor Santos <vitorpedrosasantos@gmail.com>
2022-09-05 19:42:25 -0300

    Imported ConnectDB class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import  router  from    "./routes/routes"
 import  "reflect-metadata"
-import  "./database/connect"
+import  ConnectDB from    "./database/connect"
 
 
 const   app =   express()
 const   port:   number  =   3000
+
+
+const   db  =   new ConnectDB
 
 
 app.use(express.json())
@@ -15,3 +18,4 @@ app.use(router)
 app.listen(port,    ()=>{console.log(`Server started at http://localhost:${port}`)})
 
 
+db.connection()

3208e8fbd5e87d7234850f9dfe7bc00dfea7ab05
Vitor Santos <vitorpedrosasantos@gmail.com>
2022-09-13 19:29:34 -0300

    Imported ConnectDBJS and connected to database

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import express from "express";
 import  router  from    "./routes/routes"
 import  "reflect-metadata"
-import  ConnectDB from    "./database/connect"
+import  ConnectDBJS from    "./database/connect.js"
 
 
 const   app =   express()
 const   port:   number  =   3000
 
 
-const   db  =   new ConnectDB
+const   db  =   new ConnectDBJS
 
 
 app.use(express.json())
 app.use(router)
 
 
-app.listen(port,    ()=>{console.log(`Server started at http://localhost:${port}`)})
+app.listen(port,    ()=>{
+    console.log(`Server started at ${process.env.NODE_ENV}`)
+})
 
 
 db.connection()

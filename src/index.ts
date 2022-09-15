d3f31412d5beb609fc0f5ff6718762ddf7f8798f
Vitor Santos <vitorpedrosasantos@gmail.com>
2022-09-14 22:31:48 -0300

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,23 @@ import express from "express";
 import  router  from    "./routes/routes"
 import  "reflect-metadata"
 import  ConnectDBJS from    "./database/connect.js"
+import  dotenv from    "dotenv"
 
 
 const   app =   express()
 const   port:   number  =   3000
-
-
 const   db  =   new ConnectDBJS
 
+dotenv.config()
+
+db.connection()
 
 app.use(express.json())
 app.use(router)
+
+app.use("/api/users",   router)
 
 
 app.listen(port,    ()=>{
     console.log(`Server started at ${process.env.NODE_ENV}`)
 })
-
-
-db.connection()

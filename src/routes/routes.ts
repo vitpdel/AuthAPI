c859fd55fd32b207f5b3359a1949f8ef102e5fd3
Vitor Santos <vitorpedrosasantos@gmail.com>
2022-09-15 08:56:07 -0300

    Updated routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,14 @@
+import  userController  from    "../controllers/userController"
+import  protect from    "../middleware/auth"
 import { Router } from "express";
 
+
 const   router  =   Router()
+
+
+
+router.route("/").post(userController.create)
+router.route("/login").post(userController.login)
+router.route("/:id").put(protect,   userController.update)
 
 export  default router
